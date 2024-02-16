2d4694453167329bb16484536c413bf5c0af0a3f
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-02-16 20:37:20 -0300

    Definindo nomeclatura, tamanho, visibilidade do tabuleiro.

---
+++ campo_minado_swing/src/br/com/zhant/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,14 @@
+package br.com.zhant.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.zhant.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel{
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+	
+}
---
+++ campo_minado_swing/src/br/com/zhant/cm/visao/TelaJogo.java
@@ -0,0 +1,26 @@
+package br.com.zhant.cm.visao;
+
+import javax.swing.JFrame;
+
+import br.com.zhant.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaJogo extends JFrame{
+
+	public TelaJogo() {
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		 
+		add(new PainelTabuleiro(tabuleiro));
+		
+		setTitle("Campo minado");//Define o titulo que aparecerá na barra do app;
+		setSize(690, 438);//Define o tamanho.
+		setLocationRelativeTo(null);//Define a posição que o programa abrirá
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);//Encerra o projeto após ser clicado no X do app;
+		setVisible(true);//Define a visibilidade
+		
+	}
+	
+	public static void main(String[] args) {
+		new TelaJogo();
+	}
+}
